2abdbbedf888246851d561473828bb4edd142ae5
luyang <cn_luyang@163.com>
2025-11-16 00:05:16 +0900

    no message

--- luyang-cloud-business-plan/src/main/java/io/github/luyang/business/plan/calendar/beans/CalendarDomain.java
+++ luyang-cloud-business-plan/src/main/java/io/github/luyang/business/plan/calendar/beans/CalendarDomain.java
@@ -3,16 +3,16 @@ package io.github.luyang.business.plan.calendar.beans;
 import io.github.luyang.business.plan._common.enums.db.CalendarVisibility;
 
 /**
- * @param calendarId 日历ID
- * @param visibility 日历公开范围 {@link CalendarVisibility}
+ * @param calendarId         日历ID
+ * @param calendarVisibility 日历公开范围 {@link CalendarVisibility}
  * @author yang.lu
  */
 public record CalendarDomain(
 	String calendarId,
-	CalendarVisibility visibility
+	CalendarVisibility calendarVisibility
 ) {
 
 	public boolean isPrivate() {
-		return this.visibility == CalendarVisibility.PRIVATE;
+		return this.calendarVisibility == CalendarVisibility.PRIVATE;
 	}
 }
